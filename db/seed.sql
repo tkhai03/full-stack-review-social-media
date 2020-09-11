3845230e5795c1fa631336cb5d63142cc9479702
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-09-11 14:36:01 -0600

    Fixed seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,12 +1,15 @@
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
+
 create table users (
-    users_id serial primary key,
+    id serial primary key,
     email varchar(100),
-    password varchar(250)
+    hash varchar(250)
 );
 
-create table if not exists post (
-    post_id serial primary key,
-    users_id int references users(user_id),
+create table if not exists posts (
+    id serial primary key,
+    users_id int references users(id),
     content varchar(250),
     created_at date
 );
